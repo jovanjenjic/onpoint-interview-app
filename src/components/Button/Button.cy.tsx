2c4214c5dba10eada09132d65daeec47de60aca0
jovanjenjic <57072437+jovanjenjic@users.noreply.github.com>
2023-08-28 15:56:12 +0200

    Button components tests

--- src/components/Button/Button.cy.tsx
+++ src/components/Button/Button.cy.tsx
@@ -1,16 +1,71 @@
 import React from 'react';
 import Button from './Button';
+import Styleguide from '../StyleGuide/StyleGuide';
 
-const ButtonComponent = () => {
-  return <Button type="primary" text="Button" onClick={() => {}} />;
+const buttonText = 'Submit';
+const alertText = 'Button clicked!';
+
+const ButtonComponent = ({ type }: { type: 'primary' | 'secondary' }) => {
+  return (
+    <>
+      <Styleguide />
+      <Button type={type} text={buttonText} onClick={() => alert(alertText)} />
+    </>
+  );
 };
 
-describe('Table', () => {
+describe('Button primary', () => {
   beforeEach(() => {
-    cy.mount(<ButtonComponent />);
+    cy.mount(<ButtonComponent type="primary" />);
   });
 
-  it('should render the button', () => {
-    cy.get('button').should('exist').should('have.text', 'Button');
+  it('displays the button', () => {
+    cy.get('button').should('be.visible');
+  });
+
+  it('has a blue background when type is primary', () => {
+    cy.get('button')
+      .should('have.css', 'background-color')
+      .and('eq', 'rgb(44, 139, 255)');
+  });
+
+  it('displays the correct text in the button', () => {
+    cy.get('button').should('contain', buttonText);
+  });
+
+  it('calls the onClick handler when clicked', () => {
+    cy.get('button').click();
+
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal(alertText);
+    });
+  });
+
+  it('changes background color on hover', () => {
+    cy.get('button')
+      .trigger('mouseover')
+      .should('have.css', 'background-color', 'rgb(44, 139, 255)');
+  });
+});
+
+describe('Button secondary', () => {
+  beforeEach(() => {
+    cy.mount(<ButtonComponent type="secondary" />);
+  });
+
+  it('displays the button', () => {
+    cy.get('button').should('be.visible');
+  });
+
+  it('has a gray background when type is secondary', () => {
+    cy.get('button')
+      .should('have.css', 'background-color')
+      .and('eq', 'rgb(200, 205, 212)');
+  });
+
+  it('changes background color on hover', () => {
+    cy.get('button')
+      .trigger('mouseover')
+      .should('have.css', 'background-color', 'rgb(200, 205, 212)');
   });
 });
